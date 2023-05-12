b68dae69127e2ad69627fd608e05a87e230164fe
ajoseph <ajoseph@ilsos.gov>
2023-05-12 14:00:55 -0500

    5122023 - SDJPA final commit

--- src/main/java/test/aj/restcrudjpa/dao/EmpJpaRepo.java
+++ src/main/java/test/aj/restcrudjpa/dao/EmpJpaRepo.java
@@ -1,6 +1,8 @@
 package test.aj.restcrudjpa.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import test.aj.restcrudjpa.entity.Employee;
-//test
+
+@RepositoryRestResource(path="members")
 public interface EmpJpaRepo extends JpaRepository<Employee, Integer>{};
